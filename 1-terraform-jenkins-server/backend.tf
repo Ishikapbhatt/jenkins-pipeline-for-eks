9ba09eabd04976162b3e50a6ec1b0fbc6fed533c
Ishika Bhatt <ishikap1998@gmail.com>
2025-03-05 13:26:44 +0530

    Update backend.tf

--- 1-terraform-jenkins-server/backend.tf
+++ 1-terraform-jenkins-server/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "jenkins-app-kub-2025"
+    bucket = "jenkins-app-kub-2025-1"
     region = "us-east-1"
     key = "jenkins-server/terraform.tfstate"
   }
